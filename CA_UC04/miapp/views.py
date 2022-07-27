c3db36bb0198b8b3bd6ee49381782756892c1c50
Carlos Cosme <danielcosme99@gmail.com>
2022-07-27 11:31:59 -0500

    Parte 3 - Registrar Estudiantes - OK

--- CA_UC04/miapp/views.py
+++ CA_UC04/miapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render, HttpResponse
 from miapp import models
+from miapp.models import Estudiante
 
 # Create your views here.
 layout = """"""
@@ -17,3 +18,19 @@ def saludo(request):
         'mensaje':'Proyecto para la Unidad de Competencia 04 (UC04)'
     })
 
+def crear_estudiante(request):
+ estudiante = Estudiante(
+                        codigo = "85481516",
+                        dni = "65155621",
+                        nombre = "Juan",
+                        apepat = "Betancurt",
+                        apemat = "Yanos",
+                        direccion = "SJM jr caminos del inca",
+                        telefono = "9141815454",
+                        estado = "A"
+ )
+ estudiante.save()
+ return HttpResponse (f"<h1>Estudiante registrado:</h1> "+
+ f"<br> <b>Código:</b> {estudiante.codigo} <br> <b>DNI:</b> {estudiante.dni} <br> <b>Nombre:</b> {estudiante.nombre} <br> "+
+ f"<b>Apellido paterno:</b> {estudiante.apepat} <br> <b>Apellido materno:</b> {estudiante.apemat} <br><b>Dirección:</b> "+
+ f" {estudiante.direccion} <br> <b>Telefono:</b> {estudiante.telefono} <br> <b>Estado:</b> {estudiante.estado}")
